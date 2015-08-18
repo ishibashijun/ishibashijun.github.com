0046ace5e540ad6dd5733f5f3f532a166e17022f
ijun <ishibashijun@gmail.com>
2015-08-18 07:17:46 -0700

    updated: trying to change sidebar's height to window height

--- pyHTMLParser/js/script.js
+++ pyHTMLParser/js/script.js
@@ -1,10 +1,17 @@
+var sidebar = document.getElementById("sidebar");
+
 function onResize() {
-	
+	sidebar.style.height = window.innerHeight + "px";
 }
 
 function onOrientationChange() {
-	
+	sidebar.style.height = window.innerHeight + "px";
+}
+
+function onLoad() {
+    sidebar.style.height = window.innerHeight + "px";
 }
 
 window.addEventListener("resize", onResize, false);
 window.addEventListener("orientationchange", onOrientationChange, false);
+window.addEventListener("load", onLoad, false);
